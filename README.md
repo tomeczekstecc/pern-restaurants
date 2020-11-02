0b4c751ba877920250c6fa02d7be35dd419e7bf6
Tomasz Steć <tomeczekstecc@gmail.com>
2020-11-02 23:12:52 +0100

    d

---
+++ README.md
@@ -0,0 +1,4 @@
+# SOURCE
+
+https://youtu.be/7qAXvOFhlDc
+https://youtu.be/NjYsXuSBZ5U
---
+++ cheat sheet.sql
@@ -0,0 +1,51 @@
+--help--
+-- \?
+
+--show databases
+-- \c
+
+-- CREATE DATABASE test;
+
+-- list tables \d
+
+-- info table \d [nameOfTable]
+
+CREATE TABLE products (
+  id INT,
+  name VARCHAR(50),
+  price INT,
+  on_sale boolean
+);
+
+CREATE TABLE restaurants (
+  id INT,
+  name VARCHAR(50),
+  location VARCHAR(50),
+  price_range INT
+);
+
+INSERT INTO restaurants(id, name, location,price_range) values(123,'mcdonalds', 'New York',3);
+
+CREATE TABLE restaurants (
+  id BIGSERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL
+);
+
+
+INSERT INTO restaurants(name, location,price_range) values('mcdonalds', 'New York',3),('wendys', 'Denver',2);
+
+CREATE TABLE restaurants (
+  id BIGSERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL check(price_range >=1 and price_range<=5)
+);
+
+CREATE TABLE restaurants (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL check(price_range >=1 and price_range<=5)
+);
