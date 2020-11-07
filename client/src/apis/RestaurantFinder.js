1d6a137370eac42893dd9112b0d4cab60461d3f8
Tomasz Steć <tomeczekstecc@gmail.com>
2020-11-07 22:45:02 +0100

    prepered for deploy

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
+// let baseURL;
+// process.env.NODE_ENV === 'development'
+//   ? (baseURL = 'http://localhost:4000/api/v1/restaurants')
+//   : (baseURL = '/api/v1/restaurants');
+// console.log(process.env.NODE_ENV);
+
+const baseURL =
+  process.env.NODE_ENV === 'prooduction'
+    ? 'api/v1/restaurants'
+    : 'http://localhost:4000/api/v1/restaurants';
+
 export default axios.create({
-  baseURL: 'http://localhost:4000/api/v1/restaurants',
+  baseURL,
 });
